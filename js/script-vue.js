4490633e55cb01145cac279576925638430037a4
GianluGiarde <gianlugiarde@gmail.com>
2023-05-09 16:59:27 +0200

    Add addtodo w error check & remove error

--- js/script-vue.js
+++ js/script-vue.js
@@ -4,21 +4,39 @@ const app = Vue.createApp({
             todoArr: [
                 {
                     text: "Fare la spesa",
-                    done: true
+                    done: true,
                 },
                 {
                     text: "Finire questo esercizio",
-                    done: false
+                    done: false,
                 },
             ],
+            newTodo: "",
+            inputValidity: false,
         }
     },
     methods: {
         checkTodo(i, todo) {
-            console.log("fatto");
             console.log(todo);
             this.todoArr[i].done = !todo;
         },
+        addTodo() {
+            const newTodoObj = {
+                text: this.newTodo.trim(),
+                done: false
+            };
+
+            if (newTodoObj.text.length >= 5) {
+                this.todoArr.unshift(newTodoObj);
+                this.inputValidity = false;
+            } else {
+                this.inputValidity = true;
+            }
+            this.newTodo = "";
+        },
+        removeError() {
+            this.inputValidity = false;
+        }
     },
 })
 
